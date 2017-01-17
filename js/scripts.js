eae8e76e3dc4880095de47b52f4592168e3d8716
Jason Brown <jasontbrown99@gmail.com>
2017-01-17 12:38:50 -0800

    Add simple html for puzzle setup. in js, using fill() method to replace vowels with dashes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
-"Believe you can and you're halfway there. Theodore Roosevelt"
+var puzzle = ("Believe you can and you're halfway there. Theodore Roosevelt");
 
 var vowels = ['e','i', 'o', 'u', 'a'];
+vowels.fill("-");
+
+
+
+
+// var vowelsForDashes = [];
+// vowels.forEach(function() {
+//   vowelsForDashes.
+// });
